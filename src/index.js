df6ffdd8b96f445dedb8e2a8a878175087c266fe
jacky lau <jackie217330@gmail.com>
2024-12-10 10:27:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import _TestWorker from './queueTaskWorker/testWorker'
-
-export {
-  initialize,
-  addTask,
-  getTasksAll,
-  clearTasks,
-  useTaskCount,
-  getNumOfTasksByType,
-} from './queueTaskWorker'
+import * as M from './queueTaskWorker'
 
 export const TestWorker = _TestWorker;
+export const initialize = M.initialize;
+export const addTask = M.addTask;
+export const getTasksAll = M.getTasksAll;
+export const clearTasks = M.clearTasks;
+export const useTaskCount = M.useTaskCount;
+export const getNumOfTasksByType = M.getNumOfTasksByType;
