d7d2359892045d019f3a48f0dfb7ab7a0fa493cf
jacky lau <jackie217330@gmail.com>
2025-04-22 21:52:38 +0800

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,35 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Text} from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
-import {addTask, getTasksAll, initialize, useTaskCount} from 'rn-queue-tasks';
+import {initialize} from 'rn-queue-tasks';
 
 const db = SQLite.openDatabase({
   name: '1.db',
 });
 
 export default () => {
+  const [manager, setManager] = useState(undefined);
+  const [count, setCount] = useState(0)
+  
   useEffect(() => {
-    initialize(db, []);
+    initialize(db, []).then(setManager);
   }, []);
-  const taskCount = useTaskCount();
 
   return (
     <>
-      <Text>Task Count: {taskCount}</Text>
+      <Text>Task Count: {count}</Text>
       <Button
         title={'Add Task'}
-        onPress={() =>
-          addTask({
-            type: 'test',
-            payload: {a: new Date().getTime()},
-          })
-        }
-      />
-      <Button
-        title={'Get Tasks'}
-        onPress={() =>
-          getTasksAll().then(tasks => alert(JSON.stringify(tasks, null, 2)))
-        }
+        onPress={() => {
+          manager.addTask({type: 'test', payload: {time: Date.now()}});
+        }}
       />
     </>
   );
